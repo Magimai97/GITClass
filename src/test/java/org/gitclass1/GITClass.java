629c06c21fe6b3cf070af575c6934a6bc6416ef9
Magimai97 <francistinktink@gmail.com>
2022-04-02 17:14:53 +0530

    Modified class

--- src/test/java/org/gitclass1/GITClass.java
+++ src/test/java/org/gitclass1/GITClass.java
@@ -1,5 +1,24 @@
 package org.gitclass1;
 
 public class GITClass {
+	
+	public void method() {
+		System.out.println("1");
+
+	}
+	
+	public void method1() {
+		System.out.println("2");
+
+	}
+	
+	public void method2() {
+		System.out.println("3");
+	} 
+	public void method3() {
+		System.out.println("4");
 
 }
+	}
+
+
